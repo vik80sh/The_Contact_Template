78998abd4057cedef4a8fe7eeb7413cca2930dee
Vikash <Vik80sh@gmail.com>
2020-09-17 17:29:34 +0530

    first commit

---
+++ build/precache-manifest.88b6c8bcc1662021cce5ef0dff31fcc5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65d2a2470a54115a455d067fb058523a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bdd354d1d0654997d53c",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "af460cce251cca5dee8d",
+    "url": "/static/js/2.e08b9b32.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e08b9b32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bdd354d1d0654997d53c",
+    "url": "/static/js/main.197a223c.chunk.js"
+  },
+  {
+    "revision": "5b9eb446b53ba1479f2a",
+    "url": "/static/js/runtime-main.56c69188.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
